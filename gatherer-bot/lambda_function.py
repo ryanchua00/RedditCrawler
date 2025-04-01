c8b9bd21bfaad3dc4bd5c92c5a6efa9522654222
ryanchua00 <ryanchuahj@gmail.com>
2025-04-01 15:12:35 +0800

    Add documentation and clean code for gatherer-bot/lambda-function

--- gatherer-bot/lambda_function.py
+++ gatherer-bot/lambda_function.py
@@ -4,11 +4,21 @@ import boto3
 import json
 import os
 
+# Get environment variables and aws lambda client
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 lambda_client = boto3.client('lambda')
 
 
 def sendMessage(chat_id, message):
+    """
+    The function `sendMessage` sends a message to a specified chat ID using the Telegram API.
+
+    :chat_id:
+    The Telegram unique identifier for the chat where you want to send the message.
+
+    :message:
+    The text string that will be sent to the specified chat.
+    """
     reply = {
         "chat_id": chat_id,
         "text": message
@@ -20,15 +30,27 @@ def sendMessage(chat_id, message):
 
 
 def sendDocument(chat_id, data, filename="reddit_memes.pdf"):
+    """
+    The function `sendDocument` sends a document to a specified chat ID using the Telegram API.
+
+    :chat_id:
+    The Telegram unique identifier for the chat where you want to send the document.
+
+    :data:
+    The base64 encoded pdf data that will be sent to the specified chat.
+
+    :filename:
+    Optional text string that the pdf will be named as.
+    """
     pdf_bytes = base64.b64decode(data)
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendDocument"
     files = {
         "document": (filename, pdf_bytes, "application/pdf")
     }
-    data = {
+    chat_data = {
         "chat_id": chat_id
     }
-    response = requests.post(url, files=files, data=data)
+    response = requests.post(url, files=files, data=chat_data)
 
     print(f"*** sendDocument Response : {response.json()}")
 
@@ -39,19 +61,22 @@ def lambda_handler(event, context):
     chat_id = event['message']['from']['id']
     user_name = event['message']['from']['username']
     message_text = event['message']['text']
-    # /getmemes /getreport
+
+    print(f"*** chat id: {chat_id}")
+    print(f"*** user name: {user_name}")
+    print(f"*** message text: {message_text}")
+
+    # Filter response based on message
     if message_text == "/getmemes":
-        # Call scrape-memes (synchronously)
+        # Call scrape-memes
         response_get = lambda_client.invoke(
             FunctionName='scrape-memes',
             InvocationType='RequestResponse'
         )
-        memes_data = response_get['Payload'].read().decode('utf-8')
-        print(f"*** memes_data: {memes_data}")
         sendMessage(chat_id, "Top daily memes gathered 👍")
     elif message_text == "/getreport":
-        # Call get-memes
         sendMessage(chat_id, "Generating a report... 📝")
+        # Call get-memes
         response_get = lambda_client.invoke(
             FunctionName='get-memes',
             InvocationType='RequestResponse'
@@ -60,13 +85,9 @@ def lambda_handler(event, context):
         report_data = json.loads(response_payload)
         sendDocument(chat_id, report_data["body"])
     else:
+        # Default response
         message_text = "Hello! Use /getmemes followed by /getreport to get started."
         sendMessage(chat_id, message_text)
-
-    print(f"*** chat id: {chat_id}")
-    print(f"*** user name: {user_name}")
-    print(f"*** message text: {message_text}")
-    print(event)
 
     return {
         'statusCode': 200,
